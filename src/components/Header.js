d7bff7578a207892edee0d0ffc35aee6c1ad3181
johnpolacek <johnpolacek@gmail.com>
2019-04-05 05:38:42 -0500

    code formatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,33 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import { Header, H1, H2 } from 'styled-system-html'
+import React from "react"
+import { Link } from "gatsby"
+import { Header, H1, H2 } from "styled-system-html"
 
-export default (props) => (
-  <>{
-    props.path === '/' ? (
+export default props => (
+  <>
+    {props.path === "/" ? (
       <Header textAlign="center" pb={4}>
-        <H1 pb={2} mb={2}>{props.title}</H1>
-        <H2 fontSize={2} fontWeight="normal">{props.description}</H2>
+        <H1 pb={2} mb={2}>
+          {props.title}
+        </H1>
+        <H2 fontSize={2} fontWeight="normal">
+          {props.description}
+        </H2>
       </Header>
     ) : (
-      <Header borderBottom="solid 2px" borderColor="gray2" textAlign="center" pt={2} pb={4} mb={4} fontSize={1}>
-        <H1 fontSize={2} pb={2}>{props.title}</H1>
+      <Header
+        borderBottom="solid 2px"
+        borderColor="gray2"
+        textAlign="center"
+        pt={2}
+        pb={4}
+        mb={4}
+        fontSize={1}
+      >
+        <H1 fontSize={2} pb={2}>
+          {props.title}
+        </H1>
         <Link to={`/`}>view all events</Link>
       </Header>
-    )
-  }</>
+    )}
+  </>
 )
